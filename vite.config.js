bbaf998927147d58d8f9dcc302e09edaa25cde8f
Anum Fatima <anumraza8194@gmail.com>
2024-09-18 00:49:20 +0500

    node-modules to git ignore

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
 
-
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import inspect from 'vite-plugin-inspect';
 
 export default defineConfig({
-  plugins: [react()],
-  base: '/Todo-App/' 
+  plugins: [inspect()],
+    base: '/Todo-App/' 
 });
-
-
